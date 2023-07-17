312b6343afdb7eea1f7308d4b8679c77511f8c2d
MartaKMB <muszkamarta@interia.pl>
2023-07-17 08:29:59 +0200

    initial commit for repo with js and algorithms exercises

---
+++ teoria-java-script-samuraj-programowania/operatory.js
@@ -0,0 +1,15 @@
+let position = 2;
+console.log("inkrementacja | dekrementacja", position, --position, position++, position, position += 10);
+
+let newPosition = 0;
+newPosition += position;
+console.log("newPosition", newPosition);
+
+console.log("modulo | reszta z dzielenia", 10 % 9);
+
+console.log("konwersja niejawna przy porównaniu", 1 == true, 2 == "2", 2 == true, 1 === true);
+console.log("operator róne od: ", 100 != 101, !("coś"), !!(2-2));
+
+console.log("lub ||", true || false, 2 || 5, 0 || "coś");
+console.log("i &&", true && false, (3>2 && 5>3));
+
---
+++ teoria-java-script-samuraj-programowania/zmienne.js
@@ -0,0 +1,26 @@
+let firstLetter;
+console.log(firstLetter);
+firstLetter = "J";
+console.log(firstLetter);
+
+let showUserName = function () {console.log("funkcja");}
+showUserName();
+
+let nameTest, age, adress;
+nameTest = "imię";
+age = 12;
+let arrowFunction = (a) => console.log(a);
+arrowFunction("nauka start");
+
+let example1 = 200;
+example1 = example1 + 1;
+
+const example2 = 200;
+// example2++;
+
+console.log(example1, example2);
+
+const arrayExample = [1];
+// arrayExample = [1,2]; // error
+arrayExample[1] = 2;
+console.log(arrayExample);
