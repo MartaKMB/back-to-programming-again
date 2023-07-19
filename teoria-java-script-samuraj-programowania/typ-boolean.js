d3480e0db0691372709d9a2fe4df4fa049068a19
MartaKMB <muszkamarta@interia.pl>
2023-07-19 09:31:19 +0200

    added exercises with type boolean

---
+++ teoria-java-script-samuraj-programowania/typ-boolean.js
@@ -0,0 +1,5 @@
+console.log('sprawdzanie Boolean: ', Boolean(0), Boolean( 2 >= 2), Boolean({}));
+console.log('bolean porównianie i konwersja: ', "1" == true, 1 == "1", 1 === "1", 1 === 1);
+console.log('negacja logiczna: ', !false, !!false, !0, !!0);
+console.log('operator logiczny I: ', "" && 1, 102 && 100, false && 2 < 5, 2 < 5 && 3 > 2);
+console.log('operator logiczny LUB: ', "" || 1, 102 || 100, false || 2 < 5, 2 < 5 || 3 > 2);
