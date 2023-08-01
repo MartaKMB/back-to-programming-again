9c49e55898aa5724df3341795a1aeeb4cfe2efca
MartaKMB <muszkamarta@interia.pl>
2023-08-01 11:23:08 +0200

    added validation to form

--- react-tutorials/src/AppForm.js
+++ react-tutorials/src/AppForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function AppForm() {
@@ -7,8 +7,29 @@ function AppForm() {
   const [userPassword, setUserPassword] = useState('');
   const [userAccept, setUserAccept] = useState(false);
 
+  const [userNameError, setUserNameError] = useState(false);
+  const [userEmailError, setUserEmailError] = useState(false);
+  const [userPasswordError, setUserPasswordError] = useState(false);
+  const [userAcceptError, setUserAcceptError] = useState(false);
+
+  const [formMessage, setFormMessage] = useState('');
+
+  const messages = {
+    userNameIncorect:
+      'Błędna nazwa uzytkownika - powinna mieć min. 10 znaków i nie zawierać spacji',
+    userEmailIncorect: 'Błędny email - brak @ w adresie',
+    userPasswordIncorect: 'Błędne hasło - powinno mieć dokładnie 8 znaków',
+    userAcceptIncorect: 'Brak potwierdzenia zgody - jest wymagana',
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      formMessage !== '' && setFormMessage('');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [formMessage]);
+
   const handleChange = (e) => {
-    console.log('handleChange', e.target.type, e.target.value);
     const targetName = e.target.name;
     switch (targetName) {
       case 'userName':
@@ -30,7 +51,66 @@ function AppForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Zatrwierdź: ', e);
+
+    const validation = formValidation();
+
+    if (validation.correct) {
+      setUserName('');
+      setUserEmail('');
+      setUserPassword('');
+      setUserAccept(false);
+      setUserNameError(false);
+      setUserEmailError(false);
+      setUserPasswordError(false);
+      setUserAcceptError(false);
+      setFormMessage('Formularz został wysłany!');
+    } else {
+      setUserNameError(!validation.userNameValidation);
+      setUserEmailError(!validation.userEmailValidation);
+      setUserPasswordError(!validation.userPasswordValidation);
+      setUserAcceptError(!validation.userAcceptValidation);
+    }
+  };
+
+  const formValidation = () => {
+    let userNameValidation = false;
+    let userEmailValidation = false;
+    let userPasswordValidation = false;
+    let userAcceptValidation = false;
+    let correct = false;
+
+    if (userName.length > 10 && userName.indexOf(' ') === -1) {
+      userNameValidation = true;
+    }
+
+    if (userEmail.indexOf('@') !== -1) {
+      userEmailValidation = true;
+    }
+
+    if (userPassword.length === 8) {
+      userPasswordValidation = true;
+    }
+
+    if (userAccept) {
+      userAcceptValidation = true;
+    }
+
+    if (
+      userAcceptValidation &&
+      userEmailValidation &&
+      userPasswordValidation &&
+      userAcceptValidation
+    ) {
+      correct = true;
+    }
+
+    return {
+      userNameValidation,
+      userEmailValidation,
+      userPasswordValidation,
+      userAcceptValidation,
+      correct,
+    };
   };
 
   return (
@@ -38,9 +118,6 @@ function AppForm() {
       <header>
         <strong>Tutorial: Formularz z walidacją</strong>
       </header>
-      <div style={{ display: 'none' }}>
-        Tutorial zrobiony do: 86 lekcja, 17 minuta
-      </div>
       <div>Źródło: https://www.udemy.com/course/kurs-react-od-podstaw/</div>
       <form onSubmit={handleSubmit} noValidate>
         <label htmlFor='user'>
@@ -52,6 +129,7 @@ function AppForm() {
             value={userName}
             onChange={handleChange}
           />
+          {userNameError && <span>{messages.userNameIncorect}</span>}
         </label>
         <label htmlFor='email'>
           Twoje adres email:
@@ -62,9 +140,10 @@ function AppForm() {
             value={userEmail}
             onChange={handleChange}
           />
+          {userEmailError && <span>{messages.userEmailIncorect}</span>}
         </label>
         <label htmlFor='password'>
-          Twoje adres email:
+          Twoje hasło:
           <input
             type='password'
             id='password'
@@ -72,6 +151,7 @@ function AppForm() {
             value={userPassword}
             onChange={handleChange}
           />
+          {userPasswordError && <span>{messages.userPasswordIncorect}</span>}
         </label>
         <label htmlFor='accept'>
           <input
@@ -81,9 +161,11 @@ function AppForm() {
             onChange={handleChange}
           />
           Wyrazam zgodę na wszystko!
+          {userAcceptError && <span>{messages.userAcceptIncorect}</span>}
         </label>
         <button>ZAPISZ SIĘ</button>
       </form>
+      {formMessage && <h3>{formMessage}</h3>}
     </div>
   );
 }
