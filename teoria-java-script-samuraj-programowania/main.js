e70234a3257b7fb59e713d7db6396019ab9a9cd7
MartaKMB <muszkamarta@interia.pl>
2023-08-09 16:01:10 +0200

    play with colorful console logs

--- teoria-java-script-samuraj-programowania/main.js
+++ teoria-java-script-samuraj-programowania/main.js
@@ -1,1 +1,6 @@
 console.log('main.js');
+
+console.log('%c Kolorowy console log', 'color: MediumSpringGreen');
+console.log('%c Kolorowy console log', 'color: MediumTurquoise');
+console.log('%c Kolorowy console log', 'color: LightCoral');
+console.log('%c Kolorowy console log', 'color: tomato');
