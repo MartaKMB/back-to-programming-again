0fe7c2548ac348af6cccb725f547c6149670171f
MartaKMB <muszkamarta@interia.pl>
2023-08-31 12:30:34 +0200

    added async await examples

--- teoria-java-script-samuraj-programowania/asynchronicznosc.js
+++ teoria-java-script-samuraj-programowania/asynchronicznosc.js
@@ -46,3 +46,90 @@ Promise.all([promise, promise2, promise3])
   .finally(() => console.log('Promise all final'));
 
 console.log('after promise');
+
+function lessonCallback(lessonNumber, callback) {
+  console.log(`I am learning lesson ${lessonNumber}`);
+  setTimeout(() => {
+    callback();
+  }, 2000);
+}
+
+lessonCallback(1, () => {
+  lessonCallback(2, () => {
+    lessonCallback(3, () => {
+      console.log('lessonCallback');
+    });
+  });
+});
+
+function lessonPromise(lessonNumber) {
+  return new Promise((resolve, reject) => {
+    console.log(`I am learning lesson ${lessonNumber}`);
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+}
+
+lessonPromise(1)
+  .then(() => lessonPromise(2))
+  .then(() => {
+    console.log('lessonPromise');
+    return lessonPromise(3);
+  });
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    console.log('wysłany request');
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+  });
+}
+
+fetchData('https://jsonplaceholder.typicode.com/users/10')
+  .then((data) => console.log('DATA: ', data))
+  .catch((err) => console.log('ERROR: ', err))
+  .finally(() => console.log('zakończono'));
+
+async function lessonAsyncAwait(lessonNumber) {
+  await lessonPromise(1);
+  await lessonPromise(2);
+  await lessonPromise(3);
+  console.log('koniec lessonAsyncAwait');
+}
+
+lessonAsyncAwait();
+
+async function fetchDataAsyncAwait() {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/users/10'
+    );
+    const users = await response.json();
+    console.log('fetchDataAsyncAwait USERS: ', users);
+  } catch (error) {
+    console.log('fetchDataAsyncAwait ERROR: ', error);
+  }
+}
+
+fetchDataAsyncAwait();
+
+const waitForMe = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //   resolve(`Czekano na odpowiedź ${time}ms`);
+      reject(`Czekano na odrzucenie ${time}ms`);
+    }, time);
+  });
+};
+
+(async () => {
+  try {
+    const result = await waitForMe(1222);
+    console.log('RESULT: ', result);
+  } catch (err) {
+    console.log('ERROR: ', err);
+  }
+})();
