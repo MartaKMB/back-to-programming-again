e63c77007fe7e44387bc2b7a209e90a167cc865c
MartaKMB <muszkamarta@interia.pl>
2023-11-03 13:04:23 +0100

    added next examples from course SP

--- jquery-kurs-samuraj-programowania/kompedium-2023/script.js
+++ jquery-kurs-samuraj-programowania/kompedium-2023/script.js
@@ -78,4 +78,43 @@ $('button#toggle-class').click(() => {
   $('div.animation').toggleClass('border');
 });
 
-// 48min
+$('button#hide').click(() => {
+  $('div.animation').hide();
+});
+
+$('button#show').click(() => {
+  $('div.animation').show();
+});
+
+$('button#slide-up').click(() => {
+  $('div.animation').slideUp(500);
+});
+
+$('button#slide-down').click(() => {
+  $('div.animation').slideDown(500);
+});
+
+$('button#fade-in').click(() => {
+  $('div.animation').fadeIn(500);
+});
+
+$('button#fade-out').click(() => {
+  $('div.animation').fadeOut(500);
+});
+
+$('button#fade-to').click(() => {
+  $('div.animation').fadeTo(500, 0.3);
+});
+
+// $('div.animation').text('Hello');
+// $('div.animation').html('<p style="color: yellow;">Hello</p>');
+// $('div.animation')
+//   .append('<p style="color: black;">Hello</p>')
+//   .prepend('<p style="color: red;">Hello</p>');
+
+// $('button#toggle-class').appendTo('div.animation');
+
+// $('div.animation').prepend('<div style="color: red;">Blablabla</div>');
+$('<p style="color: red;">Buttons:</p>').insertBefore('button:first');
+
+// 1:08:35
