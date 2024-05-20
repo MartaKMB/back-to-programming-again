2d5400dedf3081053c5bf2f51f7c3bf9933d21ee
MartaKMB <muszkamarta@interia.pl>
2024-05-20 16:32:48 +0200

    add example with HTMLInputElement value type string

--- ts-tutorial-zp-yt/src/app.ts
+++ ts-tutorial-zp-yt/src/app.ts
@@ -1,4 +1,10 @@
-let age: number = 29;
+const input1Element: HTMLInputElement = document.querySelector("#input1");
+const input2Element: HTMLInputElement = document.querySelector("#input2");
+const addBtn = document.querySelector("button");
 
-let ageAsString: string = "dwadzieścia dziewięć lat";
-ageAsString = `${age} lat`;
+const add = (v1: number, v2: number) => v1 + v2;
+
+addBtn.addEventListener("click", () => {
+    const sum = add(Number(input1Element.value), Number(input2Element.value));
+    console.log(sum);
+});
