627690d4241426bc530d75d7c5f8d0bebf7b64bf
MartaKMB <muszkamarta@interia.pl>
2024-06-10 09:03:24 +0200

    sligthly modify for numbersapi to continue with tutorial

--- next-net-ninja-yt/app/tickets/TicketsList.jsx
+++ next-net-ninja-yt/app/tickets/TicketsList.jsx
@@ -1,8 +1,5 @@
 async function getTickets() {
-  const res = await fetch(
-    'http://numbersapi.com/random/year?json'
-    // const res = await fetch('http://localhost:4000/tickets'
-  );
+  const res = await fetch('http://numbersapi.com/random/year?json');
   return res.json();
 }
 
@@ -10,17 +7,13 @@ export default async function TicketsList() {
   const tickets = await getTickets();
   return (
     <>
-      {/* {tickets.map((ticket) => { */}
       <div key={tickets.number} className='card my-5'>
-        {/* <div key={ticket.id} className='card my-5'> */}
-        {/* <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div> */}
-        {tickets.text}
+        <h3>{tickets.number}</h3>
+          <p>{tickets.text.slice(0, 200)}...</p>
+          <div className={`pill ${tickets.type}`}>
+            {tickets.type} type
+          </div>
       </div>
-      ;{/* })} */}
       {tickets.length === 0 && (
         <p className='text-center'>There are no open Tickets, yay!</p>
       )}
